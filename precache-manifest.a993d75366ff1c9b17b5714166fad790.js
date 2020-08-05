f6c19f9a845fdbe7f31b3e97f8c5ccc176710971
ibrahimMohamed1990 <himamohamed1991@gmail.com>
2020-08-05 14:44:29 +0200

    Updates

---
+++ precache-manifest.a993d75366ff1c9b17b5714166fad790.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07cf720be2a03cc0903eee55d09cfc00",
+    "url": "/weather-app-react/index.html"
+  },
+  {
+    "revision": "8751d90f5fa8eb39833f",
+    "url": "/weather-app-react/static/css/main.31c83d92.chunk.css"
+  },
+  {
+    "revision": "518d2f1a6913674506c6",
+    "url": "/weather-app-react/static/js/2.fb4c55bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app-react/static/js/2.fb4c55bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8751d90f5fa8eb39833f",
+    "url": "/weather-app-react/static/js/main.0371ba7d.chunk.js"
+  },
+  {
+    "revision": "73ec9f2eed65a5d44827",
+    "url": "/weather-app-react/static/js/runtime-main.4ee40ab0.js"
+  }
+]);
